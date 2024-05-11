649434091e65e593bc77e8cddb660751fef0646b
paycenonoli <119795130+paycenonoli@users.noreply.github.com>
2024-05-11 01:42:53 -0500

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "ovia-terra.pem" 
+  key_name      = "lsf-ovia-keys" 
   tags = {
     Name = var.instance_name
   }
